9c8e5f8c99eb38a4ba0ac532ef253a017e584ae4
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2024-04-17 12:47:31 +0300

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+//var generateName = require('sillyname');
+//const superheroes = require('superheroes');
+import generateName from "sillyname";
+import superheroes from "superheroes";
+
+let sillyName = generateName();
+
+console.log(`My name is: ${sillyName}.`);
+console.log(`My name is ${superheroes.random()}`);

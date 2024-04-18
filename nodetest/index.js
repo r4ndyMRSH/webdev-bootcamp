b9b27d77d21e66581faf4b3f8c61cf16b382d3fb
r4ndyMRSH <111009961+r4ndyMRSH@users.noreply.github.com>
2024-04-18 13:17:52 +0300

    Add files via upload

---
+++ nodetest/index.js
@@ -0,0 +1,15 @@
+//simple server on node.js
+
+import http from "http";
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello, World!</h1>");
+});
+
+server.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
+});

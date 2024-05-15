f2fbd94272c53a859df40e04ef7da4e46e686690
Pavel Nazaranka <pvlnzr@gmail.com>
2024-05-15 11:36:02 +0300

    Solution

--- 5.2 JSON/index.js
+++ 5.2 JSON/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
@@ -11,16 +12,36 @@ const recipeJSON =
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+let data;
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    recipe: data,
+  });
+  //console.log(data);
 });
 
 app.post("/recipe", (req, res) => {
-  //Step 3: Write your code here to make this behave like the solution website.
-  //Step 4: Add code to views/index.ejs to use the recieved recipe object.
   
-  res.render("index.ejs");
+
+  switch (req.body.choice) {
+    case "chicken":
+      data = JSON.parse(recipeJSON)[0];
+      break;
+    case "beef":
+      data = JSON.parse(recipeJSON)[1];
+      break;
+    case "fish":
+      data = JSON.parse(recipeJSON)[2];
+      break;
+
+    default:
+      break;
+  }
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {

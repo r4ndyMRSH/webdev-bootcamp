473dbd9b9cbc07b2b4de9f8a95ba2794625d057c
pvlnzr <pvlnzr@gmail.com>
2024-06-07 15:08:51 +0300

    My Solution

--- 5.4 API Authentication/index.js
+++ 5.4 API Authentication/index.js
@@ -1,31 +1,50 @@
-'use strict'
+"use strict";
 
 //https://secrets-api.appbrewery.com/
 
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com/";
 
+let response;
+let result;
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //TODO 1: Fill in your values for the 3 types of auth.
-const yourUsername = "";
-const yourPassword = "";
-const yourAPIKey = "";
-const yourBearerToken = "";
+const yourUsername = "pvlnzr";
+const yourPassword = "IAmTheBest";
+const yourAPIKey = "fee06b14-1919-4067-a3da-3f274b8a8ce1";
+const yourBearerToken = "94a8eeef-06ba-41cb-bfe8-d2e11dfef7ae";
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
 });
 
-app.get("/noAuth", (req, res) => {
+app.get("/noAuth", async (req, res) => {
   //TODO 2: Use axios to hit up the /random endpoint
   //The data you get back should be sent to the ejs file as "content"
   //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
+  try {
+    response = await axios.get("https://secrets-api.appbrewery.com/random");
+    result = JSON.stringify(response.data);
+    console.log(result);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
-app.get("/basicAuth", (req, res) => {
+app.get("/basicAuth", async (req, res) => {
   //TODO 3: Write your code here to hit up the /all endpoint
   //Specify that you only want the secrets from page 2
   //HINT: This is how you can use axios to do basic auth:
@@ -38,15 +57,51 @@ app.get("/basicAuth", (req, res) => {
       },
     });
   */
+
+  try {
+    response = await axios.get(
+      "https://secrets-api.appbrewery.com/all?page=2",
+      {
+        auth: {
+          username: yourUsername,
+          password: yourPassword,
+        },
+      }
+    );
+    result = JSON.stringify(response.data);
+    console.log(result);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
-app.get("/apiKey", (req, res) => {
+app.get("/apiKey", async (req, res) => {
   //TODO 4: Write your code here to hit up the /filter endpoint
   //Filter for all secrets with an embarassment score of 5 or greater
   //HINT: You need to provide a query parameter of apiKey in the request.
+  try {
+    response = await axios.get("https://secrets-api.appbrewery.com/filter", {
+      params: {
+        apiKey: yourAPIKey,
+        score: 5,
+      }
+    });
+    result = JSON.stringify(response.data);
+    console.log(result);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
-app.get("/bearerToken", (req, res) => {
+app.get("/bearerToken", async (req, res) => {
   //TODO 5: Write your code here to hit up the /secrets/{id} endpoint
   //and get the secret with id of 42
   //HINT: This is how you can use axios to do bearer token auth:
@@ -58,6 +113,22 @@ app.get("/bearerToken", (req, res) => {
     },
   });
   */
+  try {
+    response = await axios.get("https://secrets-api.appbrewery.com/user-secrets", {
+      headers: {
+        Authorization: `Bearer ${yourBearerToken}`
+      }
+    });
+    result = JSON.stringify(response.data);
+    console.log(result);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+
 });
 
 app.listen(port, () => {
